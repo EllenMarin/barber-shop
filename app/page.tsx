87dd78f1deda6083fb584d8d4b8d91ecf8614663
Ellen Marin <marin_ellen@hotmail.com>
2024-08-08 22:23:25 +0100

    feat: add booking item component

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,18 @@ import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
 import Image from "next/image";
 import { Card, CardContent } from "./_components/ui/card";
-import { Badge } from "./_components/ui/badge";
-import { Avatar, AvatarImage } from "./_components/ui/avatar";
 import { db } from "./_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
+import { quickSearchOptions } from "./_contants/search";
+import BookingItem from "./_components/booking-items";
 
 const Home = async () => {
-  const barbershop = await db.barbershop.findMany({});
+  const barbershops = await db.barbershop.findMany({});
+  const popularBarbershop = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  });
 
   return (
     <div>
@@ -27,6 +32,20 @@ const Home = async () => {
           </Button>
         </div>
 
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions.map((options) => (
+            <Button className="gap-2" variant="secondary" key={options.title}>
+              <Image
+                src={options.imageUrl}
+                width={16}
+                height={16}
+                alt={options.title}
+              />
+              {options.title}
+            </Button>
+          ))}
+        </div>
+
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Barber-shop"
@@ -36,42 +55,38 @@ const Home = async () => {
           />
         </div>
 
-        <h2 className="mb-3 mt-6 text-sm font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="txt-sm">Barber Shop</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col items-center border-l-2 border-solid justify-center px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">06</p>
-              <p className="text-sm">23:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         <h2 className="mb-3 mt-6 text-sm font-bold uppercase text-gray-400">
           Recomendados
         </h2>
 
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
-          {barbershop.map((barbershop) => (
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+
+        <h2 className="mb-3 mt-6 text-sm font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershop.map((barbershop) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
       </div>
+
+      <footer>
+        <Card>
+          <CardContent className="py-6 px-5">
+            <p className="text-sm text-gray-400 font-semibold">
+              2024 Copyright <span> Ellen Marin</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   );
 };
