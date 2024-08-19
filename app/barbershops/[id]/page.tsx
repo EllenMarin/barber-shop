bdbc605ad5eb2f60d44dadae1d929143eefbb054
Ellen Marin <marin_ellen@hotmail.com>
2024-08-19 19:23:50 +0100

    feat: ensure barbershop services are shown on barbershop page

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,3 +1,4 @@
+import ServiceItem from "@/app/_components/service-item";
 import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
@@ -18,6 +19,9 @@ interface BarbershopsPageProps {
       where: {
         id: params.id,
       },
+      include: {
+        services: true,
+      }
     });
 
     if (!barbershop) {
@@ -75,6 +79,14 @@ interface BarbershopsPageProps {
             <p className="text-justify text-sm">{barbershop?.description}</p>
           </div>
 
+          <div className="space-y-3 p-5">
+            <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
+            <div className="space-y-3">
+              {barbershop.services.map((service) => (
+                <ServiceItem key={service.id} service={service} />
+              ))}
+            </div>
+          </div>
         </div>
         )
     
